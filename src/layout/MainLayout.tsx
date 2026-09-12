9354f567b87a506d38e8f838f751f5ac1c7a5cb0
litnps <lalitth21.n@gmail.com>
2026-09-12 16:11:21 +0700

    add footer

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="ชื่อ-สกุล" studentId="รหัสนักศึกษา" />
+          <Footer year="2026" fullName="Lalitnapas Pasasuk" studentId="680610712" />
         </div>
       </div>
     </div>
